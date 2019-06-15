969c122f529cc3212916d4c49fa0a1976f429ae7
Stuart Mesham <stumes3@gmail.com>
2019-06-15 19:41:56 +0200

    added pen up and down methods to plotter module

--- plotter.py
+++ plotter.py
@@ -11,44 +11,50 @@ class Plotter(object):
         self.plotter_controller = PlotterController()
         self.STEPS_PER_CM = 1000 / 4
         self.file = None
-
+        
         self.restore_stw()
-
+    
     def save_stw(self):
         with open('save.txt', 'w') as self.file:
             print(self.s, file=self.file)
             print(self.t, file=self.file)
             print(self.w, file=self.file)
-
+    
     def restore_stw(self):
         with open('save.txt', 'r') as self.file:
             self.s = int(next(self.file))
             self.t = int(next(self.file))
             self.w = int(next(self.file))
-
+    
+    def pen_up(self):
+        self.plotter_controller.pen_up()
+    
+    def pen_down(self):
+        self.plotter_controller.pen_down()
+    
     def move_to_st(self, s, t):
         self.move_st(s - self.s, t - self.t)
-
+    
     def move_st(self, delta_s, delta_t):
         print(f"Moving: delta_s={delta_s}, delta_t={delta_t}")
         self.plotter_controller.move(delta_s, delta_t)
         self.s += delta_s
         self.t += delta_t
         print("s, t, w = " + str(self.get_stw_pos()))
-
+    
     def move(self, delta_x, delta_y):
         print(f"Moving: delta_x={delta_x}, delta_y={delta_y}")
         delta_s = math.floor(math.sqrt(delta_x ** 2 + delta_y ** 2))
         delta_t = math.floor(math.sqrt((self.w - delta_x) ** 2 + delta_y ** 2))
         self.move_st(delta_s, delta_t)
         print("s, t, w = " + str(self.get_stw_pos()))
-
+    
     def move_to_xy(self, x, y):
         s = int(math.sqrt(x ** 2 + y ** 2))
         t = int(math.sqrt((self.w - x) ** 2 + y ** 2))
         self.move_to_st(s, t)
         self.save_stw()
-
+    
     def calibrate(self, s=None, t=None, w=None):
         if s and t and w:
             self.s = s
@@ -57,17 +63,17 @@ class Plotter(object):
         else:
             print("Starting calibration:")
             print("Stepper S going to home.")
-
+            
             # Give the stepper a wiggle so you know which one you're working with
             self.plotter_controller.move(75, 0)
             self.plotter_controller.move(-75, 0)
-
+            
             while True:
                 amount = input("Move stepper S downwards by [integer]: ")
                 if amount == "0" or amount == "":
                     break
                 self.plotter_controller.move(int(amount), 0)
-
+            
             # Give the stepper a wiggle so you know which one you're working with
             # Give the stepper a wiggle so you know which one you're working with
             self.plotter_controller.move(0, 75)
@@ -78,23 +84,23 @@ class Plotter(object):
                 if amount == "0" or amount == "":
                     break
                 self.plotter_controller.move(0, int(amount))
-
+            
             # Now both steppers are calibrated to be zero
             self.s, self.t = 0, 0
-
+            
             while True:
                 amount = input("Move stepper S towards T by [integer]: ")
                 if amount == "0" or amount == "":
                     break
                 self.move_st(int(amount), 0)
-
+            
             self.w = self.s
         print(f"Calibration complete, s={self.s}, t={self.t}, w={self.w}")
         self.save_stw()
-
+    
     def easy_calibrate(self):
         print("Welcome to easy calibration!© We appreciate your patronage")
-
+        
         print("Move the module underneath the left stepper")
         while True:
             amount = input("Move s t:")
@@ -102,7 +108,7 @@ class Plotter(object):
                 break
             plotter.move_st(int(amount.split()[0]), int(amount.split()[1]))
         self.s = 0
-
+        
         print("Move the module underneath the right stepper")
         while True:
             amount = input("Move s t:")
@@ -113,7 +119,7 @@ class Plotter(object):
         self.t = 0
         self.save_stw()
         print(f"Calibration complete! s={self.s}, t={self.t}, w={self.w}")
-
+    
     def get_stw_pos(self):
         return self.s, self.t, self.w
 
@@ -125,7 +131,7 @@ if __name__ == '__main__':
         plotter.move_to_xy(x, 7000)
         time.sleep(0.5)
     # plotter.calibrate()
-
+    
     # while True:
     #     amount = input("s t:")
     #     if amount == "0 0" or amount == "":
@@ -136,6 +142,6 @@ if __name__ == '__main__':
     #     if amount == "0 0" or amount == "":
     #         break
     #     plotter.move_to_xy(int(amount.split()[0]), int(amount.split()[1]))
-
+    
     print("s, t, w = " + str(plotter.get_stw_pos()))
     print("Plotter Finished")
