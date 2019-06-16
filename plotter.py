18d3f90c6af7416660c74639f18f9aaf273b609b
beyarkay <boydrkane@gmail.com>
2019-06-16 21:34:13 +0200

    Large code refactor and cleanup

--- plotter.py
+++ plotter.py
@@ -13,9 +13,9 @@ class Plotter(object):
         self.file = None
         self.restore_stw()
         # Wiggle all the actuators, to check let the user know the serial connection is working
-        self.move_st(75, 75)
+        self.move_by_st(75, 75)
         self.pen_down()
-        self.move_st(-75, -75)
+        self.move_by_st(-75, -75)
         self.pen_up()
 
     def save_stw(self):
@@ -37,21 +37,14 @@ class Plotter(object):
         self.plotter_controller.pen_down()
 
     def move_to_st(self, s, t):
-        self.move_st(s - self.s, t - self.t)
+        self.move_by_st(s - self.s, t - self.t)
 
-    def move_st(self, delta_s, delta_t):
-        print(f"Moving: delta_s={delta_s}, delta_t={delta_t}")
+    def move_by_st(self, delta_s, delta_t, verbose=True):
         self.plotter_controller.move(delta_s, delta_t)
         self.s += delta_s
         self.t += delta_t
-        print("s, t, w = " + str(self.get_stw_pos()))
-
-    def move(self, delta_x, delta_y):
-        print(f"Moving: delta_x={delta_x}, delta_y={delta_y}")
-        delta_s = math.floor(math.sqrt(delta_x ** 2 + delta_y ** 2))
-        delta_t = math.floor(math.sqrt((self.w - delta_x) ** 2 + delta_y ** 2))
-        self.move_st(delta_s, delta_t)
-        print("s, t, w = " + str(self.get_stw_pos()))
+        if verbose:
+            print(f"Moving: delta_s={delta_s}, delta_t={delta_t}, (s, t, w) = {self.get_stw_pos()}")
 
     def move_to_xy(self, x, y):
         s = int(math.sqrt(x ** 2 + y ** 2))
@@ -59,7 +52,7 @@ class Plotter(object):
         self.move_to_st(s, t)
         self.save_stw()
 
-    def calibrate(self, s=None, t=None, w=None):
+    def calibrate(self, s=None, t=None, w=None, verbose=True):
         if s and t and w:
             self.s = s
             self.t = t
@@ -96,29 +89,26 @@ class Plotter(object):
                 amount = input("Move stepper S towards T by [integer]: ")
                 if amount == "0" or amount == "":
                     break
-                self.move_st(int(amount), 0)
+                self.move_by_st(int(amount), 0, verbose=verbose)
 
             self.w = self.s
         print(f"Calibration complete, s={self.s}, t={self.t}, w={self.w}")
         self.save_stw()
 
-    def easy_calibrate(self):
+    def easy_calibrate(self, verbose=True):
         print("Welcome to easy calibration!© We appreciate your patronage")
 
         print("Move the module underneath the left stepper")
-        while True:
-            amount = input("Move s t:")
-            if amount == "0 0" or amount == "":
-                break
-            plotter.move_st(int(amount.split()[0]), int(amount.split()[1]))
+        amount = input("s t:")
+        while amount != "":
+            # Assume default of zero if only one argument is given
+            amount += " 0"
+            s, t = int(amount.split()[0]), int(amount.split()[1])
+            self.plotter_controller.move(s, t)
         self.s = 0
 
-        print("Move the module underneath the right stepper")
-        while True:
-            amount = input("Move s t:")
-            if amount == "0 0" or amount == "":
-                break
-            plotter.move_st(int(amount.split()[0]), int(amount.split()[1]))
+        print("Now move the module underneath the right stepper")
+        self.control_using_st()
         self.w = self.s
         self.t = 0
         self.save_stw()
@@ -127,6 +117,32 @@ class Plotter(object):
     def get_stw_pos(self):
         return self.s, self.t, self.w
 
+    def control_using_st(self, verbose=True):
+        amount = input("s t:")
+        while amount != "":
+            # Assume default of zero if only one argument is given
+            amount = amount + " 0"
+            s, t = int(amount.split()[0]), int(amount.split()[1])
+            self.move_by_st(s, t, verbose=verbose)
+
+    def control_using_xy(self, verbose=True):
+        amount = input("x y:")
+        while amount != "":
+            # Assume default of zero for the second argument if only one argument is given
+            amount = amount + " 0"
+            x, y = int(amount.split()[0]), int(amount.split()[1])
+            self.move_by_st(x, y, verbose=verbose)
+
+    def control_from_cmd_line(self, verbose=True):
+        mode = input("Control the module using st or xy coordinates? [xy/st]: ")
+
+        while mode in ["xy", "st"]:
+            if mode == "st":
+                self.control_using_st(verbose=verbose)
+            if mode == "xy":
+                self.control_using_xy(verbose=verbose)
+            mode = input("Control the module using st or xy coordinates? [xy/st]: ")
+
 
 if __name__ == '__main__':
     plotter = Plotter()
@@ -134,21 +150,11 @@ if __name__ == '__main__':
     if "y" == input("s, t, w = " + str(plotter.get_stw_pos()) + ", Do you want to calibrate?[y/n]: "):
         plotter.easy_calibrate()
 
+    plotter.control_from_cmd_line()
 
     # for x in range(100, 7100, 100):
     #     plotter.move_to_xy(x, 7000)
     #     time.sleep(0.5)
-
-    # while True:
-    #     amount = input("s t:")
-    #     if amount == "0 0" or amount == "":
-    #         break
-    #     plotter.move_st(int(amount.split()[0]), int(amount.split()[1]))
-    while True:
-        amount = input("x y:")
-        if amount == "0 0" or amount == "":
-            break
-        plotter.move_to_xy(int(amount.split()[0]), int(amount.split()[1]))
 
     print("s, t, w = " + str(plotter.get_stw_pos()))
     print("Plotter Finished")
